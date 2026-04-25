752488bf2805519827ef38b4938f7f2e4969627f
joshua-arnao <joshua.arnao@icloud.com>
2026-04-25 14:10:11 -0500

    feat: booking-service service interface defined

---
+++ booking-service/src/main/java/com/autodidacta/bookingservice/service/BookingService.java
@@ -0,0 +1,16 @@
+package com.autodidacta.bookingservice.service;
+
+import com.autodidacta.bookingservice.dto.BookingRequest;
+import com.autodidacta.bookingservice.dto.BookingResponse;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface BookingService {
+    BookingResponse createBooking(BookingRequest bookingRequest);
+    BookingResponse getBookingById(UUID bookingId);
+    BookingResponse confirmBooking(UUID bookingId);
+    BookingResponse cancelBooking(UUID bookingId);
+    List<BookingResponse> getBookingsByTripId(UUID bookingId);
+    BookingResponse getBookingByEmailAndBookingId(String email, UUID bookingId);
+}
