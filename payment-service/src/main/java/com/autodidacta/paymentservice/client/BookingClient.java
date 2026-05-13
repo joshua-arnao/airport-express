71e32187440036efa69c743557d9c4a590cb21c4
joshua-arnao <joshua.arnao@icloud.com>
2026-05-13 14:47:43 -0500

    feat: payment-service working with Stripe integration

--- payment-service/src/main/java/com/autodidacta/paymentservice/client/BookingClient.java
+++ payment-service/src/main/java/com/autodidacta/paymentservice/client/BookingClient.java
@@ -3,12 +3,12 @@ package com.autodidacta.paymentservice.client;
 import com.autodidacta.paymentservice.dto.BookingClientResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.UUID;
 
-@FeignClient(name = "booking-service")
+@FeignClient(name = "booking-service", url = "http://localhost:8083")
 public interface BookingClient {
-    @PostMapping("/bookings/confirm/{bookingId}")
-    BookingClientResponse confirmBooking(@PathVariable("bookingId") UUID bookingId);
+    @PutMapping("/api/bookings/{bookingId}/confirm")
+    BookingClientResponse confirmBooking(@PathVariable UUID bookingId);
 }
