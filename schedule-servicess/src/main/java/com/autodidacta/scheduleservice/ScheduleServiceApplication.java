0ef1d81f985687107b5a2b5c2a1c3446895b8d21
joshua-arnao <joshua.arnao@icloud.com>
2026-05-12 20:55:53 -0500

    feat: enable JPA auditing and Feign clients in all services

--- schedule-servicess/src/main/java/com/autodidacta/scheduleservice/ScheduleServiceApplication.java
+++ schedule-servicess/src/main/java/com/autodidacta/scheduleservice/ScheduleServiceApplication.java
@@ -2,8 +2,10 @@ package com.autodidacta.scheduleservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ScheduleServiceApplication {
 
     public static void main(String[] args) {
