4abd2da4a600da8035653687dcd27d2e52366c82
joshua-arnao <joshua.arnao@icloud.com>
2026-05-13 21:27:31 -0500

    feat: GlobalExceptionHandler added to tracking-service

---
+++ tracking-service/src/main/java/com/autodidacta/trackingservice/shared/error/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.autodidacta.trackingservice.shared.error;
+
+import com.autodidacta.trackingservice.shared.exceptions.BusPositionNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(BusPositionNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleUserNotFound(BusPositionNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new ErrorResponse(404, ex.getMessage(), LocalDateTime.now()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGeneric(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorResponse(500, "An error occurred", LocalDateTime.now()));
+    }
+}
